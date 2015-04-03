2ab237927f294596cacabdbca10f7ded814f2991
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2015-04-03 17:01:39 +0700

    change path

--- src/models/ContentMongo.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Mylife\Service\Models;
-
-use Mylife\Service\Provider\Mongodb\Model as Eloquent;
-
-class ContentMongo extends Eloquent
-{
-    protected $collection = 'playlist_content';
-
-}
